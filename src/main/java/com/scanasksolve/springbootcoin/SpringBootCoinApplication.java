e8dfcc47627470172211ddc64c54671ee9d506dc
scanAskSolve <b10430230@outlook.com>
2023-01-08 17:03:04 +0800

    first commit

---
+++ src/main/java/com/scanasksolve/springbootcoin/SpringBootCoinApplication.java
@@ -0,0 +1,13 @@
+package com.scanasksolve.springbootcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCoinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scanasksolve/springbootcoin/SpringBootCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scanasksolve.springbootcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
